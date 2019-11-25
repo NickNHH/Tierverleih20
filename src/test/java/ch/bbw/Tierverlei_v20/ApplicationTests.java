e429db922a75ebdb9f1700aba7a440234c67a374
5ia17jowaeger <5ia17jowaeger@gibwintra.ch>
2019-11-25 15:08:07 +0100

    Initail commit

---
+++ src/test/java/ch/bbw/Tierverlei_v20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.Tierverlei_v20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
